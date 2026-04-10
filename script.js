8c02a5b037e263a571c47c55b31465d07b9f9761
Hamilton <hamiltonemiel@gmail.com>
2026-04-09 23:40:08 -0400

    Initial commit - nail_sxnat website

---
+++ script.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const images = document.querySelectorAll(".gallery-grid img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+};
+
+modal.onclick = () => {
+    modal.style.display = "none";
+};
